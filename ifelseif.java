74fb9d1630a2ffab7f806ce4502b138d3b780e8e
princenaware <princenaware516@gmail.com>
2026-01-19 21:03:39 +0530

    Update ifelseif.java

--- ifelseif.java
+++ ifelseif.java
@@ -1,3 +1,9 @@
+//This program demonstrates the use of if-else-if statements in Java.
+//It checks the value of a string variable 'city' and prints a corresponding message.
+//Useful for beginners and college students.
+
+
+import java.util.*;
 public class ifelseif {   
 public static void main(String[] args) {   
 String city = "Delhi";   
